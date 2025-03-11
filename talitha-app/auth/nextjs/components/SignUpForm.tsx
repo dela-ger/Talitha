d1b787703ba61b21e0090a1fc3ccd1e6a392bf5a
gdjamehkt <gdjameh@ktdevs.com>
2025-03-11 09:51:22 +0000

    added minimal design to signUp and signIn

--- talitha-app/auth/nextjs/components/SignUpForm.tsx
+++ talitha-app/auth/nextjs/components/SignUpForm.tsx
@@ -28,30 +28,46 @@ function SignUpForm() {
 
   return (
     <>
-        <form action="" onSubmit={handleSubmit}>
-            <h2>Sign Up</h2>
-            <input 
-                type="text"
-                name='name'
-                placeholder='Full Name'
-                value={formData.name}
-                onChange={handleChange}
-            />
-            <input 
-                type="email"
-                name='email'
-                placeholder='Email'
-                value={formData.email}
-                onChange={handleChange}
-            />
-            <input 
-                type="password"
-                name='password'
-                placeholder='Password'
-                value={formData.password}
-                onChange={handleChange}
-            />
-            <button type='submit'>
+        <form className='
+        flex flex-col min-h-screen justify-center items-center
+        p-8 rounded-lg shadow-md border bg-white border-blue-500' action="" onSubmit={handleSubmit}>
+            <h2 className='text-2xl font-bold mb-6'>Sign Up</h2>
+            
+            {/* name field */}
+            <div className='flex flex-col mb-4 w-64 '>
+                <label className=''>Name</label>
+                <input className='border border-black-200'
+                    type="text"
+                    name='name'
+                    placeholder='Full Name'
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className='flex flex-col mb-4 w-64'>
+                <label className=''>Email</label>
+                <input className='border border-black-200'
+                    type="email"
+                    name='email'
+                    placeholder='Email'
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className='flex flex-col mb-4 w-64'>
+                <label className=''>Password</label>
+                <input className='border border-black-200'
+                    type="password"
+                    name='password'
+                    placeholder='Password'
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type='submit' 
+                className='w-64 bg-blue-500 text-white py-2 cursor-pointer'>
                 Register
             </button>
         </form>
