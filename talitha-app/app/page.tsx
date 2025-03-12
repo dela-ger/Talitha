d15b27e5d73cd9c806c8fab796827308d40b82ce
gdjamehkt <gdjameh@ktdevs.com>
2025-03-12 10:54:21 +0000

    Adding Clerk auth

--- talitha-app/app/page.tsx
+++ talitha-app/app/page.tsx
@@ -1,9 +1,11 @@
+import LogOutButton from "@/auth/nextjs/components/LogOutButton";
 import SignInForm from "@/auth/nextjs/components/SignInForm";
 // import SignUpForm from "@/auth/nextjs/components/SignUpForm";
 
 export default function Home() {
   return (
     <div>
+      <LogOutButton />
       <SignInForm />
     </div>
   );
