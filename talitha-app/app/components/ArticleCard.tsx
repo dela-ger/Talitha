3448e87e40d652d559a077aa0ada6d1a6df783dd
gdjamehkt <gdjameh@ktdevs.com>
2025-03-21 15:26:12 +0000

    Added the COntext for passing data

---
+++ talitha-app/app/components/ArticleCard.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react';
+import { useArticles } from '../context/ArticleContext';
+
+export default function ArticleCard() {
+  const { articles } = useArticles()
+
+  return (
+    <>
+        <div>
+          <h1 className='text-4xl'>Recent Article</h1>
+            {articles.map((article) => (
+              <div key={article.id}>
+                <h2>{article.title}</h2>
+                <p>{article.content.substring(0, 80)}...</p>
+                <p>{article.author}</p>
+              </div>
+            ) )}
+        </div>
+    </>
+  )
+}
--- talitha-app/app/page.tsx
+++ talitha-app/app/page.tsx
@@ -1,4 +1,5 @@
 // import Link from "next/link";
+import ArticleCard from "./components/ArticleCard";
 import Hero from "./components/Hero";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <>
       <div>
         <Hero />
-      
+        <ArticleCard />
       </div>
     </>
     
