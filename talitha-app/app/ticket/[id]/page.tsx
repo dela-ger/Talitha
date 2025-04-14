946d0aea3d04fa6704f7a99175fd159adc785c4b
gdjamehkt <gdjameh@ktdevs.com>
2025-04-14 11:17:30 +0000

    Added Events Page

---
+++ talitha-app/app/ticket/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TicketDetail() {
+  return (
+    <>
+        <div>Ticket Details</div>
+    </>
+  )
+}
---
+++ talitha-app/app/ticket/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { events, TicketEvent } from './ticketData'
+
+
+export default function Ticket() {
+  return (
+    <>
+        <div>
+            <h1>Up Coming Events</h1>
+        </div>
+    </>
+  )
+}
