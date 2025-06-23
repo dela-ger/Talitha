138b80209c6a1d51def8b4c3ba197a712ba2725f
gdjamehkt <gdjameh@ktdevs.com>
2025-06-23 21:55:09 +0000

    changed the product id pages

--- talitha-app/next.config.ts
+++ talitha-app/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Add this for static export
   trailingSlash: true,
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com', 'drive.google.com'],
   },
-  
 };
 
 export default nextConfig;
